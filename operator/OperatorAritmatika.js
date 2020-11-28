7170cf8d9102d3ae07f848ef5ca7b916c68fb741
setiyawan12 <yayangsetiyawan0@gmail.com>
2020-11-29 00:56:36 +0700

    start

---
+++ operator/OperatorAritmatika.js
@@ -0,0 +1,34 @@
+//operator
+//aritmatika
+
+//Dengan Metode Literal//
+console.log(12+1);
+//End//
+
+//Dengan Metode Variable//
+var nilai1 = 10;
+var nilai2 = 5;
+
+//===========================
+//Penjumlahan, Pada JavaScript, operator penjumlahan menggunakan karakter plus, +.
+console.log('====Tambah====');
+console.log(nilai1+nilai2); //Variable
+console.log(2+nilai1+nilai2); //Expression
+//============================
+//Pengurangan,Operator pengurangan menggunakan karakter minus, -.
+console.log('====Kurang====');
+console.log(nilai1-nilai2); //Variable
+console.log(2-nilai1-nilai2); //Expression
+//============================
+//Perkalian, Operator perkalian menggunakan karakter bintang, *.
+console.log('====Perkalian====');
+console.log(nilai1*nilai2); //Variable
+console.log(2*nilai1*nilai2); //Expression
+//============================
+//Pembagian, Operasi pembagian menggunakan karakter slash, /.
+console.log('====Pembagian====');
+console.log(nilai1/nilai2); //Variable
+console.log(2/nilai1/nilai2); //Expression
+
+//End//
+
