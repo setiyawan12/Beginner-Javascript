7171bed5fad43c8fbb628e96becf94434d73646a
setiyawan12 <yayangsetiyawan0@gmail.com>
2020-11-29 01:06:38 +0700

    OperatorAritmatika

--- operator/OperatorAritmatika.js
+++ operator/OperatorAritmatika.js
@@ -1,13 +1,12 @@
 //operator
 //aritmatika
-
+console.log('OPERATOR ARITMATIKA');
 //Dengan Metode Literal//
 console.log(12+1);
 //End//
 
 //Dengan Metode Variable//
-var nilai1 = 10;
-var nilai2 = 5;
+var nilai1 = 10, nilai2 = 5;
 
 //===========================
 //Penjumlahan, Pada JavaScript, operator penjumlahan menggunakan karakter plus, +.
@@ -29,6 +28,12 @@ console.log(2*nilai1*nilai2); //Expression
 console.log('====Pembagian====');
 console.log(nilai1/nilai2); //Variable
 console.log(2/nilai1/nilai2); //Expression
+//============================
+//Operasi modulus menggunakan karakter percent, %. Operasi modulus merupakan operasi untuk menghasilkan sisa, remainder, dari hasil pembagian
+console.log('====Modulus====');
+console.log(5%4); //Modulus Positif
+console.log(5%-4); //Modulus Positif
+console.log(-5%4); //Modulus Negatif
 
 //End//
 
