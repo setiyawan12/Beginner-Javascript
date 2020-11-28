fbc3b61e1d0c1c79c5327ba938421253df4067f6
setiyawan12 <yayangsetiyawan0@gmail.com>
2020-11-29 01:29:15 +0700

    Tipe Data

---
+++ TipeData/String.js
@@ -0,0 +1,17 @@
+//Tipe Data String Di JavaScript
+console.log('====Simple String=====');
+var title = 'Selamat Datang Setiyawan'
+console.log(title);
+
+//mengabungkan String
+console.log('====Mengabungkan String=====');
+var hubung = 'Plugin'
+var hubung1 = '007'
+console.log(hubung+hubung1);
+
+//kita menggunakan huruf bersar atau kapital untuk nama variabel yang terdiri dari dua suku kata
+console.log('====Membuat Nama Variable Dengan Fromat camelCase=====');
+var siteName = 'setiyawan12'; //camelCase
+var url = 'https://github.com/setiyawan12';
+var visitorCount = 5921; //camelCase
+console.log(siteName+'\n'+url+'\n'+visitorCount);
