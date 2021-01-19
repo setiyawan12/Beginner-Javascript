13ecda87c7bcdbbfce05175c294f82505e53fe81
setiyawan12 <yayangsetiyawan0@gmail.com>
2021-01-20 03:15:51 +0700

    lanjutan

---
+++ Perulangan/while.js
@@ -0,0 +1,16 @@
+let i = 0
+//while
+while (i < 5){
+    console.log("my while")
+    i++
+}
+//do while
+//syntax
+//      do {
+//      code block to be executed
+//      }
+//      while (condition);
+do{
+console.log("my do while");
+i++
+}while(i <10);
