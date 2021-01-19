620e9104e87e2a72befb426619b9d44ae412a990
setiyawan12 <yayangsetiyawan0@gmail.com>
2021-01-20 03:48:36 +0700

    1. Object Function

---
+++ Function/function.js
@@ -0,0 +1,9 @@
+//syntax
+//function myFunction(p1, p2) {
+//    return p1 * p2;   // The function returns the product of p1 and p2
+//  }
+function myFunction(){
+    return 2+1
+}
+
+console.log(myFunction())
---
+++ Object/object.js
@@ -0,0 +1,19 @@
+//Object
+let nama = {
+    //Isi dari Sebuat Object
+    namaDepan : "yayang",
+    namaBelakang : "setiyawan"
+};
+console.log(nama)
+
+let nama1 = {
+    namaDepan:"yayag",
+    namaBelakang: "setiyawan"
+};
+console.log(nama1.namaDepan);
+console.log(nama1.namaBelakang);
+
+let person ={
+    nama:"YayangSetiyawan", umur:20
+}
+console.log("nama saya = " + person.nama + "\numur saya = " + person.umur);
